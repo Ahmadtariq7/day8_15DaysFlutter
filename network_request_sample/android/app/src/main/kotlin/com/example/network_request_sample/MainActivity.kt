685b6ac88a2416b1bbd126871aaaa04a2d574a36
Ahmad Tariq <ahmad.tariq2528@gmail.com>
2022-03-22 23:03:50 +0500

    Network Requests
    Day 8 :: Flutter 15 Days Challenge
    
    Concepts Covered: Widgets, JSON, JSON Parsing, Handling API Requests, http, get function

---
+++ network_request_sample/android/app/src/main/kotlin/com/example/network_request_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.network_request_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ network_request_sample/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:network_request_sample/dataService.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _dataService = DataService();
+  String _response;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Builder(
+            builder: (context) {
+              if (_response != null) {
+                return Text(_response);
+              } else {
+                return ElevatedButton(
+                  child: Text("Make Request"),
+                  onPressed: _makeRequest,
+                );
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _makeRequest() async {
+    final response = await _dataService.makeRequestToApi();
+    setState(() => _response = response);
+  }
+}
